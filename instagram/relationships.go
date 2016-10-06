14b5bf288d86384ca3d6026c854a084f337e5333
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2016-10-06 14:44:15 -0700

    Adds :

--- instagram/relationships.go
+++ instagram/relationships.go
@@ -205,7 +205,7 @@ func relationshipAction(s *RelationshipsService, userID, action, method string) 
 	}
 
 	rel := new(Relationship)
-	res, err = s.client.Do(req, rel)
+	res, err := s.client.Do(req, rel)
 	log.Errorf("%+v\n", res)
 	return rel, err
 }
